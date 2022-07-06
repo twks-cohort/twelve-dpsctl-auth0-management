7360d9dbdb897f7925ffd7e75d656b257c68f0f4
Patrick McFadden <pemcfadden@gmail.com>
2022-07-06 12:15:23 -0600

    [pmcfadden] Removing testing on writing to op

--- scripts/deploy_cli_auth0_application.py
+++ scripts/deploy_cli_auth0_application.py
@@ -31,13 +31,16 @@ else:
   response = requests.post(url, headers=header, json=body)
 
 # write client-id to secrets store
-client_id_result = os.system(f"opw write svc-auth0 {os.environ.get('TENANT')}-{os.environ.get('TENANT_APPLICATION')}-client-id-TESTING {response.json()['client_id']}")
+client_id_result = os.system(f"opw write svc-auth0 {os.environ.get('TENANT')}-{os.environ.get('TENANT_APPLICATION')}-client-id {response.json()['client_id']}")
 if client_id_result != 0:
   print("Error writing client id")
   sys.exit(1)
+
+# Bug with opw writing too quickly causes a 400
+time.sleep(1)
+
 # write client-secret to secrets store
-time.sleep(3)
-client_secret_result = os.system(f"opw write svc-auth0 {os.environ.get('TENANT')}-{os.environ.get('TENANT_APPLICATION')}-client-secret-TESTING {response.json()['client_secret']}")
+client_secret_result = os.system(f"opw write svc-auth0 {os.environ.get('TENANT')}-{os.environ.get('TENANT_APPLICATION')}-client-secret {response.json()['client_secret']}")
 if client_secret_result != 0:
   print("Error writing client secret")
   sys.exit(1)

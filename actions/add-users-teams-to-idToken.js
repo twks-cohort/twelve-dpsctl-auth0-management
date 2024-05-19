3bdc6106bd97685a309b3bb7f5be95189271561c
lanzhao <lanzhao@thoughtworks.com>
2024-05-19 17:41:44 +0800

    update context tenant dev info

--- actions/add-users-teams-to-idToken.js
+++ actions/add-users-teams-to-idToken.js
@@ -1,4 +1,4 @@
 exports.onExecutePostLogin = async (event, api) => {
-  const namespace = 'https://github.org/ThoughtWorks-DPS/';
+  const namespace = 'https://github.com/orgs/twks-cohort/';
   api.idToken.setCustomClaim(namespace + 'teams', event.user.app_metadata.roles);
 }
